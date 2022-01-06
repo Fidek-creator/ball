4c556832a36ec603de7462ad38a4b6d686901411
Fidek-creator <83137313+Fidek-creator@users.noreply.github.com>
2022-01-06 15:52:18 +0100

    init

---
+++ Assets/Scripts/AddPlayerVelocity.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddPlayerVelocity : MonoBehaviour {
+
+	[SerializeField]
+	Vector3 v3Force;
+	[SerializeField]
+	KeyCode keyPositive;
+	[SerializeField]
+	KeyCode keyNegative;
+
+	private Vector3 spawn;
+
+	void Start()
+    {
+		spawn = transform.position;
+	}
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(Input.GetKey(keyPositive))
+			GetComponent<Rigidbody>().velocity += v3Force;
+		if (Input.GetKey(keyNegative))
+			GetComponent<Rigidbody>().velocity -= v3Force;
+		
+	}
+	void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.tag == "Enemy")
+        {
+			transform.position = spawn;
+        }
+    }
+
+    [System.Obsolete]
+    void OnTriggerEnter(Collider other)
+    {
+		if(other.transform.tag == "Goal") 
+		{
+			Manager.CompleteLevel();
+		}
+    }
+}
---
+++ Assets/Scripts/Manager.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manager : MonoBehaviour {
+
+    public static int currentLevel=0;
+
+    // Use this for initialization
+    [System.Obsolete]
+    public static void CompleteLevel()
+    {
+        currentLevel += 1;
+        Application.LoadLevel(currentLevel);
+    }
+}
